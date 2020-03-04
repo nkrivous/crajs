0e50af46b5da61656c840442b196dfd97ad648cc
nkrivous <kolsln@list.ru>
2020-03-04 11:33:52 +0300

    Use different ways in updating components with array

--- src/components/RendersMapObject.js
+++ src/components/RendersMapObject.js
@@ -6,27 +6,60 @@ const initialState = [
   [3, "three"]
 ];
 
-export class RendersMapObject extends React.Component {
+export class RendersMapObject extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.editArray = this.editArray.bind(this);
+  }
   state = {
-    model: new Map(initialState)
+    modelMap: new Map(initialState),
+    modelArray: [...initialState]
   };
 
-  edit = key => {
+  editMap = key => () => {
     this.setState(state => {
-      state.model.set(key, "new value");
-      return { model: state.model };
+      state.modelMap.set(key, "new value");
+      return { modelMap: new Map(state.modelMap) };
+    });
+  };
+
+  editArray = function(event) {
+    const key = +event.target.getAttribute("data-key");
+    this.setState(state => {
+      const item = state.modelArray.find(item => item[0] === key);
+      item[1] = "new value";
+      return { modelArray: state.modelArray };
     });
   };
 
   render() {
-    const { model } = this.state;
+    const { modelMap, modelArray } = this.state;
     return (
       <div>
+        <h2>Map:</h2>
         {initialState.map(x => {
           const [key, value] = x;
           return (
-            <div key={key} onClick={() => this.edit(key)}>
-              {model.get(key)}
+            <div key={key} onClick={this.editMap(key)}>
+              {modelMap.get(key)}
+            </div>
+          );
+        })}
+        <h2>Array from initialState:</h2>
+        {initialState.map(x => {
+          const [key, value] = x;
+          return (
+            <div key={key} data-key={key} onClick={this.editArray}>
+              {modelArray.find(item => item[0] === key)[1]}
+            </div>
+          );
+        })}
+        <h2>Array:</h2>
+        {modelArray.map(x => {
+          const [key, value] = x;
+          return (
+            <div key={key} data-key={key} onClick={this.editArray}>
+              {value}
             </div>
           );
         })}
